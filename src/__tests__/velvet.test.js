77c51ddc4b96c46faa1707b0e1f0c848ac73cf12
Chad Gerein <chadifferus@gmail.com>
2024-12-06 07:59:10 -0400

    removing old tests

--- src/__tests__/velvet.test.js
+++ src/__tests__/velvet.test.js
@@ -2,4 +2,13 @@ describe('velvet', () => {
     it('should run', () => {
         expect(1).toEqual(1);
     })
+    it('should run a second time', () => {
+        expect(1).toEqual(1);
+    })
+    it('should run a third time', () => {
+        expect(1).toEqual(1);
+    })
+    it('should run the first time', () => {
+        expect(1).toEqual(1);
+    })
 });
